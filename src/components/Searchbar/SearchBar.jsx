463f13f8f789c51961ec69e187b9c5aa8e1032d2
Ofir David <ofir.david30@gmail.com>
2024-02-23 19:04:02 -0500

    json static file results fetched

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,19 +1,26 @@
 import {FaSearch} from 'react-icons/fa'
 import './SearchBar.css'
 import { useEffect, useState } from 'react'
+import drugs from '../../Drugs/Drugs.json'
 export default function SearchBar(){
-    const [input,setInput] = useState();
+    const [input,setInput] = useState("");
 
-    const fetchData = (value)=>{
+    const fetchData = (value) => {
+        if (value.length >= 3) {
+          return drugs['drug-name'].filter(name => 
+            name.toUpperCase().includes(value.toUpperCase())
+          );
+        }
+        return [];
+      };
+    const handleChange = (value)=>{
         setInput(value)
+        console.log(fetchData(value));
     }
-    useEffect(()=>{
-        console.log(input)
-    },[input])
     return(
         <div className="input-wrapper">
             <FaSearch id="search-icon"/>
-            <input placeholder='Type to search...' value={input} onChange={e => fetchData(e.target.value)}/>
+            <input placeholder='Type to search...' value={input} onChange={e => handleChange(e.target.value)}/>
         </div>
     )
 }

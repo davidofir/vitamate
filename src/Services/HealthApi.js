5e4967f6e910f8781e442c8f86020ea2263eb530
Ofir David <ofir.david30@gmail.com>
2024-03-02 17:51:02 -0500

    data fetching and displaying functional

---
+++ src/Services/HealthApi.js
@@ -0,0 +1,24 @@
+
+const baseUrl = 'https://api.fda.gov/drug/label.json?search=';
+const findFieldByKeyContaining = (data, keyword) => {
+    const key = Object.keys(data).find(key => key.toLowerCase().includes(keyword.toLowerCase()));
+    return key ? data[key] : 'Not available';
+};
+export const fetchDrugs = async (drugName) => {
+    const res = await fetch(`${baseUrl}${encodeURI(drugName)}`);
+    if (!res.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await res.json();
+    const fetchedData = data.results[0]
+    console.log(fetchedData)
+    return {
+        'purpose': findFieldByKeyContaining(fetchedData, 'purpose'),
+        'warnings': findFieldByKeyContaining(fetchedData, 'warnings'),
+        'do_not_use': findFieldByKeyContaining(fetchedData, 'do_not_use'),
+        'usage': findFieldByKeyContaining(fetchedData, 'indications_and_usage'),
+        'dosage': findFieldByKeyContaining(fetchedData, 'dosage_and_administration'),
+        'ask_doctor': findFieldByKeyContaining(fetchedData, 'ask_doctor_or_pharmacist'),
+        'questions': findFieldByKeyContaining(fetchedData, 'questions')
+    }
+};
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -5,42 +5,80 @@ import SearchResultsList from '../../components/SearchResultsList/SearchResultsL
 import Stack from 'react-bootstrap/Stack';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import {fetchDrugs} from '../../Services/HealthApi';
+
 function Search(){
     const [results, setResults] = useState([]);
     const [selectedResults, setSelectedResults] = useState([]);
+    const [drugName, setDrugName] = useState('');
+    const [drugData,setDrugData] = useState();
+    
     const existingValues = {};
+    let data;
+    useEffect(()=>{
+      (async()=>{
+        if(drugName.length > 0){
+          data = await fetchDrugs(drugName);
+          setDrugData(data)
+        }
+        
+      })()
+    },[drugName])
+    const renderDrugData = () => {
+      if (!drugData) {
+          return <div>Please Select a Drug</div>;
+      }
+
+      return (
+          <div>
+              {Object.entries(drugData).map(([key, value]) => (
+                  <div key={key}>
+                      <strong>{key}:</strong> {value}
+                  </div>
+              ))}
+          </div>
+      );
+  };
     return(
         <div className="parent-search">
             <div className="search-bar-container">
                 <SearchBar setResults={setResults}/>
                 <SearchResultsList results={results} setSelectedResults={setSelectedResults} selectedResults={selectedResults}/>
                 <Stack>
-  {selectedResults.map((result, id) => {
-    if (!existingValues[result]) {
-      existingValues[result] = true;
-      return (
-        <div className="selected-result-item" key={id}>
-            {result}    
-          
-          <Button 
-            className="remove-button"
-            style={{borderRadius:'10px'}} 
-            onClick={(e) => {
-              e.stopPropagation();
-              setSelectedResults(selectedResults.filter(item => item !== result));
-            }}
-            aria-label={`Remove ${result}`}
-          >
-            X
-          </Button>
-        </div>
-      );
-    }
-  })}
-</Stack>
-            </div>
-
-        </div>
+                  {selectedResults.map((result, id) => {
+                    if (!existingValues[result]) {
+                      existingValues[result] = true;
+                      return (
+                        <div className="selected-result-item" key={id}>
+                            <div onClick={(e)=>{
+                                e.stopPropagation();
+                                setDrugName(e.target.textContent)
+                            }}>
+                                {result}
+                            </div>
+                                
+                          
+                          <Button 
+                            className="remove-button"
+                            style={{borderRadius:'10px'}} 
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              setSelectedResults(selectedResults.filter(item => item !== result));
+                            }}
+                            aria-label={`Remove ${result}`}
+                          >
+                            X
+                          </Button>
+                        </div>
+                      );
+                    }else{
+                      return <div></div>
+                    }
+                  })}
+            </Stack>
+                </div>
+                {renderDrugData()}
+                  </div>
         );
 }
 export default Search;

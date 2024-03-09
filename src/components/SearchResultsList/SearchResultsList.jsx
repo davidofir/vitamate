dfc3dcc47e2f36dee5a029b0387cd4c489fb7454
Ofir David <ofir.david30@gmail.com>
2024-03-09 18:54:06 -0500

    prevent duplications

--- src/components/SearchResultsList/SearchResultsList.jsx
+++ src/components/SearchResultsList/SearchResultsList.jsx
@@ -5,7 +5,9 @@ export default function({results,setSelectedResults,selectedResults}){
         <div className="results-list" >
             {
                 results.map((result,id)=>{
-                    return <SearchResultItem result={result} key={id} setSelectedResults={setSelectedResults} selectedResults={selectedResults}/>
+                        return <SearchResultItem result={result} key={id} setSelectedResults={setSelectedResults} selectedResults={selectedResults}/>
+                    
+                    
                 })
             }
         </div>
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -5,93 +5,101 @@ import SearchResultsList from '../../components/SearchResultsList/SearchResultsL
 import Stack from 'react-bootstrap/Stack';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import {fetchDrugs} from '../../Repositories/DrugsRepository';
+import { fetchDrugs } from '../../Repositories/DrugsRepository';
 import Modal from 'react-bootstrap/Modal';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Card from 'react-bootstrap/Card';
-function Search(){
-  const [results, setResults] = useState([]);
-  const [selectedResults, setSelectedResults] = useState([]);
-  const [drugName, setDrugName] = useState('');
-  const [drugData, setDrugData] = useState();
-  const stripHtml = (htmlString) => {
-    const temporalDivElement = document.createElement("div");
-    temporalDivElement.innerHTML = htmlString;
-    let text = temporalDivElement.textContent || temporalDivElement.innerText || "";
-  
-    text = text.replace('•', '\n•');
-  
-    return text;
-  };
-  useEffect(() => {
-      (async () => {
-          if (drugName.length > 0) {
-            try{
-                const data = await fetchDrugs(drugName);
-                setDrugData(data);
+function Search() {
+    const [results, setResults] = useState([]);
+    const [selectedResults, setSelectedResults] = useState([]);
+    const [drugName, setDrugName] = useState('');
+    const [drugData, setDrugData] = useState();
+    const existingResults = {};
+    const stripHtml = (htmlString) => {
+        const temporalDivElement = document.createElement("div");
+        temporalDivElement.innerHTML = htmlString;
+        let text = temporalDivElement.textContent || temporalDivElement.innerText || "";
+
+        text = text.replace('•', '\n•');
+
+        return text;
+    };
+    useEffect(() => {
+        (async () => {
+            if (drugName.length > 0) {
+                try {
+                    const data = await fetchDrugs(drugName);
+                    setDrugData(data);
+                }
+                catch (e) {
+                    setDrugData({
+                        'Drug not found': 'Please try a different drug'
+                    })
+                }
+
             }
-            catch(e){
-                setDrugData({
-                    'Drug not found':'Please try a different drug'
-                })
-            }
+        })();
+    }, [drugName]);
 
-          }
-      })();
-  }, [drugName]);
+    const renderDrugDataTabs = () => {
+        if (!drugData) {
+            return <div></div>;
+        }
 
-  const renderDrugDataTabs = () => {
-      if (!drugData) {
-          return <div></div>;
-      }
+        return (
+            <Tabs defaultActiveKey="first" className="mb-3">
+                {Object.entries(drugData).map(([key, value], index) => (
+                    <Tab eventKey={key} title={key} key={index}>
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <Card style={{ padding: '10px', width: '80%', height: '80%' }}>
+                                {stripHtml(value)}
+                            </Card>
+                        </div>
+                    </Tab>
+                ))}
+            </Tabs>
+        );
+    };
 
-      return (
-          <Tabs defaultActiveKey="first" className="mb-3">
-              {Object.entries(drugData).map(([key, value], index) => (
-                  <Tab eventKey={key} title={key} key={index}>
-                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                      <Card style={{padding:'10px',width:'80%',height:'80%'}}>
-                      {stripHtml(value)}
-                      </Card>
-                    </div>
-                  </Tab>
-              ))}
-          </Tabs>
-      );
-  };
+    return (
+        <div className="parent-search">
+            <div className="search-bar-container">
+                <SearchBar setResults={setResults} />
+                {results ? <SearchResultsList results={results} setSelectedResults={setSelectedResults} selectedResults={selectedResults} /> : <></>}
+                <Stack>
 
-  return (
-      <div className="parent-search">
-          <div className="search-bar-container">
-              <SearchBar setResults={setResults} />
-              {results ? <SearchResultsList results={results} setSelectedResults={setSelectedResults} selectedResults={selectedResults}/> : <></> } 
-              <Stack>
-                  {selectedResults.map((result, id) => (
-                      <div className="selected-result-item" key={id}>
-                          <div onClick={() => {setDrugName(result)}}>
-                              {result}
-                          </div>
-                          <Button
-                              className="remove-button"
-                              style={{ borderRadius: '10px' }}
-                              onClick={(e) => {
-                                  e.stopPropagation();
-                                  setSelectedResults(selectedResults.filter(item => item !== result));
-                              }}
-                              aria-label={`Remove ${result}`}
-                          >
-                              X
-                          </Button>
-                      </div>
-                  ))}
-              </Stack>
-          </div>
-          <div>
-            {renderDrugDataTabs()}
-          </div>
+                    {selectedResults.map((result, id) => {
+                        if (!existingResults[result]) {
+                            existingResults[result] = true;
+                            return (
+                                <div className="selected-result-item" key={id}>
+                                    <div onClick={() => { setDrugName(result) }}>
+                                        {result}
+                                    </div>
+                                    <Button
+                                        className="remove-button"
+                                        style={{ borderRadius: '10px' }}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            setSelectedResults(selectedResults.filter(item => item !== result));
+                                        }}
+                                        aria-label={`Remove ${result}`}
+                                    >
+                                        X
+                                    </Button>
+                                </div>
+                            );
+                        }
+                        return null;
+                    })}
+                </Stack>
+            </div>
+            <div>
+                {renderDrugDataTabs()}
+            </div>
 
-      </div>
-  );
+        </div>
+    );
 }
 export default Search;
